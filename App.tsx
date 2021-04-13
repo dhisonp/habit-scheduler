74a8e721ecb73ca37fb1f880d6ba295323970585
dhisonp <67208312+dhisonp@users.noreply.github.com>
2021-04-12 21:10:14 -0400

    Second commit

--- App.tsx
+++ App.tsx
@@ -1,7 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import { NavigationContainer, StackActions } from '@react-navigation/native';
+import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import HomeScreen from './src/HomeScreen';
 
@@ -10,7 +10,14 @@ const Stack = createStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      <Stack.Navigator screenOptions={{
+        headerShown: false,
+        headerStyle: {
+          backgroundColor: 'white',
+          elevation: 0,
+          shadowOpacity: 0,
+        }
+      }}>
         <Stack.Screen name="Home" component={HomeScreen} />
       </Stack.Navigator>
     </NavigationContainer>
--- src/HomeScreen.tsx
+++ src/HomeScreen.tsx
@@ -7,8 +7,13 @@ interface HomeScreenProps { }
 const HomeScreen = (props: HomeScreenProps) => {
     return (
         <View style={styles.container}>
-            <Text>Habit Scheduler</Text>
-            <StatusBar style="auto" />
+            <View style={styles.headerTitle}>
+                <Text style={styles.title}>Home</Text>
+            </View>
+            <View style={styles.subcontainer}>
+                <Text>Habit Scheduler</Text>
+                <StatusBar style="auto" />
+            </View>
         </View>
     );
 };
@@ -16,5 +21,26 @@ const HomeScreen = (props: HomeScreenProps) => {
 export default HomeScreen;
 
 const styles = StyleSheet.create({
-    container: {}
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        backgroundColor: "#fdf0ff",
+    },
+    subcontainer: {
+        // backgroundColor: 'red',
+        flex: 1,
+        marginHorizontal: 48,
+        marginVertical: 138,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    headerTitle: {
+        position: 'absolute',
+        top: 36, left: 0, bottom: 0, right: 0,
+        alignItems: 'center',
+    },
+    title: {
+        fontSize: 24,
+        fontWeight: 'bold',
+    }
 });
